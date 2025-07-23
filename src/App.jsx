1489bedf7129c0c44ace565c3d8d76d9a4360854
McDavis Albertio <mcdavis705@gmail.com>
2025-07-23 02:33:19 +0200

    Import the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/NavBar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>PIWC</h1>
+      <Navbar />
+      <div>
+        <Outlet />
+      </div>
+
+
     </>
   )
 }
