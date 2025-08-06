e265fd3f8e8eda75b42a438fc3405fbdb5aed428
McDavis Albertio <mcdavis705@gmail.com>
2025-08-06 13:41:49 +0200

    Linked the events page to NOCODB to Map over the events

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { Outlet, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/NavBar.jsx'
 import Footer from './components/Footer.jsx'
 
-import userContext from './context/userContext.js'
+import UserContext from './context/UserContext.js'
 
 import './index.css'
 import Home from './pages/user/Home.jsx'
@@ -23,29 +23,28 @@ function App() {
 
   // retrieving the data from nocodb with the token
   const userData = {
-    nombre: 'Mcdavis',
+    name: 'Mcdavis',
     token: 'omIVMg1s3VvQr4Nbe0CqK1W5fFWJHakGu4o0o-H2'
   }
 
 
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="events" element={<Events />} />
-        <Route path="sermons" element={<Sermons />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="aboutus" element={<AboutUs />} />
-        <Route path="admin" element={<Admin />} />
-        <Route path="logIn" element={<LogIn />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <div className="min-h-screen ">
-        <Outlet />
-      </div>
-
-      <Footer />
+      <h1>Test</h1>
+      <UserContext.Provider value={userData}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="events" element={<Events />} />
+          <Route path="sermons" element={<Sermons />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="aboutus" element={<AboutUs />} />
+          <Route path="admin" element={<Admin />} />
+          <Route path="logIn" element={<LogIn />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </UserContext.Provider>
     </>
   )
 }
--- src/pages/User/Events.jsx
+++ src/pages/User/Events.jsx
@@ -1,9 +1,27 @@
-import { useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
+import UserContext from "../../context/UserContext.js"
 
 
 function Events() {
-    const {token} = useContext(AuthContext);
+    const { token } = useContext(UserContext);
+    const urlApi = "https://app.nocodb.com/api/v2/tables/m2d172w9k3mf04q/records"
 
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                'xc-token': token
+            }
+        };
+
+        fetch(urlApi, options)
+            .then(resp => resp.json())
+            .then(data => setEvents(data.list))
+            .catch(err => console.log(err));
+    }, []);
 
     return (
         <section
@@ -15,7 +33,26 @@ function Events() {
                     <h1 className="mb-5 text-5xl font-bold">Events</h1>
                     <p className="mb-5">
                         Events Page with card
-
+                        {events.map(event =>
+                            <div className="card bg-base-100 w-96 shadow-sm" key={event.id}>
+                                <figure>
+                                    <img
+                                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                                        alt="Shoes" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">
+                                        {event.Title}
+                                        <div className="badge badge-secondary">{event.date}</div>
+                                    </h2>
+                                    <p>{event.description}</p>
+                                    <div className="card-actions justify-end">
+                                        <div className="badge badge-outline">Venue: {event.venue}</div>
+                                        <div className="badge badge-outline">Time: {event.time}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </p>
                 </div>
             </div>
